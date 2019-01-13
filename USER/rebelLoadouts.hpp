40ac23663e28553a36b6707182b2e3efa9587165
Synch2k <kevin.piontek@live.de>
2019-01-13 19:49:03 +0100

    Update rebelLoadouts.hpp

--- USER/rebelLoadouts.hpp
+++ USER/rebelLoadouts.hpp
@@ -1,3 +1,8 @@
+/*
+*   Hier können eigene factions für grad-loadout eingetragen werden.
+*   Anleitung im Wiki.
+*/
+
 #ifdef NIGHTVISION
   #define NVITEM ""
 #else
@@ -22,21 +27,50 @@
 
 class orp_rebels {
 	class AllUnits {
-		uniform = "ARC_GER_Flecktarn_Uniform";
-		vest = "ARC_GER_Flecktarn_Plate_Carrier";
+		uniform[] = {
+			"U_BG_Guerilla2_3",
+		//	"U_BG_Guerilla3_1",
+			"U_BG_Guerilla2_2",
+			"U_BG_Guerilla2_1",
+			"U_BG_Guerrilla_6_1"
+		//	"U_BG_Guerilla1_1"	
+		};
+		vest[] = {
+			"V_BandollierB_blk",
+			"V_BandollierB_rgr",
+			"V_BandollierB_cbr",
+			"V_BandollierB_khk",
+			"V_BandollierB_oli"
+		};
 		backpack = "";
-		headgear = "ARC_GER_Flecktarn_Helmet_simple";
-		primaryWeapon = "rhs_weap_hk416d10";
-        primaryWeaponMagazine = "rhs_mag_30Rnd_556x45_Mk318_Stanag";
-		primaryWeaponOptics = "rhsusf_acc_g33_T1";
+		headgear[] = {
+			"H_Cap_tan",
+			"H_Cap_oli",
+			"H_Cap_grn",
+			"H_Cap_blk",
+			"V_BandollierB_oli",
+			"H_Booniehat_khk",
+			"H_Booniehat_tan",
+			"H_Booniehat_oli"
+		};
+		goggles[] = {
+			"G_Bandanna_beast",
+			"G_Bandanna_blk",
+			"G_Bandanna_khk",
+			"G_Bandanna_oli"
+		};
+		
+		primaryWeapon = "";	
+        primaryWeaponMagazine = "";
+		primaryWeaponOptics = "";
 		primaryWeaponPointer = LLITEM;
 		primaryWeaponMuzzle = SUPPRESSORITEM;
         primaryWeaponUnderbarrel = "";
 		primaryWeaponUnderbarrelMagazine = "";
-        secondaryWeapon = "";
-        secondaryWeaponMagazine = "";
-		handgunWeapon = "rhsusf_weap_glock17g4";
-        handgunWeaponMagazine = "rhsusf_mag_17Rnd_9x19_JHP";
+        secondaryWeapon = "rhs_weap_type94_new";
+        secondaryWeaponMagazine = "rhs_mag_6x8mm_mhp";
+		handgunWeapon = "";
+        handgunWeaponMagazine = "";
 		binoculars = "Binocular";
 		map = "ItemMap";
 		compass = "ItemCompass";
@@ -48,12 +82,9 @@ class orp_rebels {
     class Type {
         //Rifleman
         class Soldier_F {
+			primaryWeapon = "rhs_weap_kar98k";	
+			primaryWeaponMagazine = "rhsgref_5Rnd_792x57_kar98k";
             addItemsToUniform[] = {
-                LIST_1("ACE_MapTools"),
-                LIST_1("ACE_DefusalKit"),
-                LIST_2("ACE_CableTie"),
-                LIST_1("ACE_Flashlight_MX991"),
-
                 LIST_4("ACE_packingBandage"),
                 LIST_4("ACE_elasticBandage"),
                 LIST_4("ACE_quikclot"),
@@ -62,56 +93,48 @@ class orp_rebels {
                 LIST_2("ACE_epinephrine")
             };
             addItemsToVest[] = {
-                LIST_2("HandGrenade"),
-                LIST_2("SmokeShell"),
-                LIST_2("rhsusf_mag_17Rnd_9x19_JHP"),
-                LIST_7("rhs_mag_30Rnd_556x45_Mk318_Stanag")
+                LIST_3("rhsusf_mag_17Rnd_9x19_JHP"),
+                LIST_4("rhsgref_5Rnd_792x57_kar98k")
             };
         };
 
         //Asst. Autorifleman
         class soldier_AAR_F: Soldier_F {
-            backpack = "ARC_GER_Kitbag_compact_Flecktarn";
-            addItemsToBackpack[] = {
-                LIST_2("rhs_200rnd_556x45_M_SAW"),
-                "rhs_200rnd_556x45_T_SAW"
+            primaryWeapon = "rhs_weap_Izh18";
+			primaryWeaponMagazine = "rhsgref_1Rnd_00Buck";
+                addItemsToVest[] = {
+				LIST_3("rhs_mag_6x8mm_mhp"),
+                LIST_40("rhsgref_1Rnd_00Buck")
             };
         };
 
         //Autorifleman
         class soldier_AR_F: Soldier_F {
-            primaryWeapon = "rhs_weap_m249_pip_S";
-            primaryWeaponMagazine = "rhs_200rnd_556x45_T_SAW";
-            handgunWeapon = "";
-            handgunWeaponMagazine = "";
-            backpack = "ARC_GER_Kitbag_compact_Flecktarn";
-            addItemsToBackpack[] = {
-                LIST_2("rhs_200rnd_556x45_M_SAW"),
-                "rhsusf_100Rnd_556x45_soft_pouch",
-            };
+            primaryWeapon = "rhs_weap_scorpion";
+            primaryWeaponMagazine = "rhsgref_10rnd_765x17_vz61";
+			
             addItemsToVest[] = {
-                LIST_2("HandGrenade"),
-                LIST_2("SmokeShell")
+				LIST_3("rhs_mag_6x8mm_mhp"),
+                LIST_4("rhsgref_10rnd_765x17_vz61")
             };
         };
 
         //Combat Life Saver
         class medic_F: Soldier_F {
-            headgear = "ARC_GER_Flecktarn_Helmet_simple_Medic";
-            backpack = "ARC_GER_Backpack_Flecktarn_Med";
-            addItemsToUniform[] = {
-                "ACE_MapTools",
-                "ACE_DefusalKit",
-                LIST_2("ACE_CableTie"),
-                "ACE_Flashlight_MX991"
-            };
-            addItemsToVest[] = {
-                LIST_2("HandGrenade"),
-                LIST_2("SmokeShellPurple"),
-                LIST_8("SmokeShell"),
-                LIST_2("rhsusf_mag_17Rnd_9x19_JHP"),
-                LIST_8("rhs_mag_30Rnd_556x45_Mk318_Stanag")
-            };
+			
+			uniform[] = {
+			"U_BG_Guerilla2_3",
+			"U_BG_Guerilla2_2"
+			};
+            headgear = "H_Bandanna_camo";
+            backpack = "ga_dlc_low_messenger_olive_medic";
+            handgunWeapon = "rhsusf_weap_m1911a1";
+			handgunWeaponMagazine = "rhsusf_mag_7x45acp_MHP";
+            primaryWeapon = "";	
+			primaryWeaponMagazine = "";
+			addItemsToVest[] = {
+                LIST_6("rhsusf_mag_7x45acp_MHP"),
+			}
             class Rank {
                 class PRIVATE {
                     GRAD_FACTIONS_MEDICITEMS_CFR
@@ -129,63 +152,34 @@ class orp_rebels {
             };
         };
 
-        //Explosive Specialist
-        class soldier_exp_F: Soldier_F {
-            headgear = "ARC_GER_Flecktarn_Helmet_EOD";
-            backpack = "ARC_GER_Kitbag_compact_Flecktarn";
-            addItemsToBackpack[] = {
-                "ACE_Clacker",
-                "ACE_M26_Clacker",
-                "SatchelCharge_Remote_Mag",
-                LIST_5("DemoCharge_Remote_Mag")
-            };
-        };
+        //Team Leader
+        class Soldier_TL_F: Soldier_F {
+			headgear = "H_Beret_blk";
+			uniform = "U_BG_Guerrilla_6_1";
+			
+			primaryWeapon = "rhs_weap_m3a1";	
+			primaryWeaponMagazine = "rhsgref_30rnd_1143x23_M1911B_SMG";
+			addItemsToVest[] = {
+                LIST_1("rhsusf_mag_17Rnd_9x19_JHP"),
+                LIST_2("rhsgref_30rnd_1143x23_M1911B_SMG")
+			};
+			
 
-        //Grenadier
-        class Soldier_GL_F: Soldier_F {
-            primaryWeapon = "rhs_weap_hk416d10_m320";
-            addItemsToVest[] = {
-                LIST_2("HandGrenade"),
-                LIST_2("SmokeShell"),
-                LIST_2("rhsusf_mag_17Rnd_9x19_JHP"),
-                LIST_8("rhs_mag_30Rnd_556x45_Mk318_Stanag"),
-                LIST_8("1Rnd_HE_Grenade_shell"),
-                LIST_3("1Rnd_SmokeRed_Grenade_shell")
-            };
-        };
-
-        //Rifleman (AT)
-        class soldier_LAT_F: Soldier_F {
-            secondaryWeapon = "rhs_weap_M136";
         };
 
         //Squad Leader
-        class Soldier_SL_F: Soldier_F {
-            backpack = "tf_rt1523g_big_bwmod";
-            addItemsToBackpack[] = {
-                LIST_2("SmokeShellBlue"),
-                LIST_2("SmokeShellGreen"),
-                LIST_2("SmokeShellOrange"),
-                LIST_2("SmokeShellPurple"),
-                LIST_2("SmokeShellRed"),
-                LIST_2("SmokeShellYellow"),
-                LIST_2("SmokeShell")
-            };
-        };
+        class Soldier_SL_F: Soldier_TL_F {
+			uniform = "U_BG_leader";
+			vest = "V_Rangemaster_belt";
+			handgunWeapon = "rhsusf_weap_m1911a1";
+			handgunWeaponMagazine = "rhsusf_mag_7x45acp_MHP";
+            primaryWeapon = "";	
+			primaryWeaponMagazine = "";
+			addItemsToVest[] = {
+                LIST_6("rhsusf_mag_7x45acp_MHP"),
+			}
+			
 
-        //Team Leader
-        class Soldier_TL_F: Soldier_F {
-            primaryWeapon = "rhs_weap_hk416d10_m320";
-            addItemsToVest[] = {
-                LIST_2("HandGrenade"),
-                LIST_2("SmokeShell"),
-                LIST_2("rhsusf_mag_17Rnd_9x19_JHP"),
-                LIST_8("rhs_mag_30Rnd_556x45_Mk318_Stanag"),
-                LIST_2("1Rnd_SmokeRed_Grenade_shell"),
-                LIST_2("1Rnd_SmokeGreen_Grenade_shell"),
-                LIST_2("1Rnd_SmokeBlue_Grenade_shell"),
-                LIST_2("1Rnd_HE_Grenade_shell")
-            };
         };
     };
 };
