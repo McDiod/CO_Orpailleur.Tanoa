275baf6ae713e9bece248d02b0f30f6d88b62078
Synch2k <kevin.piontek@live.de>
2019-01-13 13:33:21 +0100

    Update enemyLoadouts.hpp

--- USER/enemyLoadouts.hpp
+++ USER/enemyLoadouts.hpp
@@ -52,8 +52,8 @@ class securite {
 		nvgoggles = NVITEM;
     };
     class Type {
-        //Rifleman
-        class Soldier_F {
+        
+        class Survivor_F {
             addItemsToUniform[] = {
                 LIST_4("ACE_packingBandage"),
                 LIST_4("ACE_elasticBandage"),
@@ -62,128 +62,83 @@ class securite {
                 LIST_2("ACE_morphine"),
                 LIST_2("ACE_epinephrine")
             };
-            addItemsToVest[] = {
-                LIST_7("6Rnd_45ACP_Cylinder")
-            };
         };
+		//POLIZEI ANFANG
+		class Soldier_F: Survivor_F {
+			primaryWeapon = "arifle_AKS_F";
+			primaryWeaponMagazine = "30Rnd_545x39_Mag_F";
+			uniform = "U_C_IDAP_Man_cargo_F";
+			vest = "V_TacVest_gen_F";
+			backpack = "";
+			headgear = "";
+			goggles = "";
+			handgunWeapon = "hgun_Rook40_F";
+			handgunWeaponMagazine = "16Rnd_9x21_Mag";
+			addItemsToVest[] = {
+                LIST_4("16Rnd_9x21_Mag")
+            };
+		}
 
-        //Asst. Autorifleman
-        class soldier_AAR_F: Soldier_F {
-            backpack = "ARC_GER_Kitbag_compact_Flecktarn";
-            addItemsToBackpack[] = {
-                LIST_2("rhs_200rnd_556x45_M_SAW"),
-                "rhs_200rnd_556x45_T_SAW"
-            };
-        };
-
-        //Autorifleman
-        class soldier_AR_F: Soldier_F {
-            primaryWeapon = "rhs_weap_m249_pip_S";
-            primaryWeaponMagazine = "rhs_200rnd_556x45_T_SAW";
-            handgunWeapon = "";
-            handgunWeaponMagazine = "";
-            backpack = "ARC_GER_Kitbag_compact_Flecktarn";
-            addItemsToBackpack[] = {
-                LIST_2("rhs_200rnd_556x45_M_SAW"),
-                "rhsusf_100Rnd_556x45_soft_pouch",
-            };
-            addItemsToVest[] = {
-                LIST_2("HandGrenade"),
-                LIST_2("SmokeShell")
-            };
-        };
-
-        //Combat Life Saver
-        class medic_F: Soldier_F {
-            headgear = "ARC_GER_Flecktarn_Helmet_simple_Medic";
-            backpack = "ARC_GER_Backpack_Flecktarn_Med";
-            addItemsToUniform[] = {
-                "ACE_MapTools",
-                "ACE_DefusalKit",
-                LIST_2("ACE_CableTie"),
-                "ACE_Flashlight_MX991"
-            };
-            addItemsToVest[] = {
-                LIST_2("HandGrenade"),
-                LIST_2("SmokeShellPurple"),
-                LIST_8("SmokeShell"),
-                LIST_2("rhsusf_mag_17Rnd_9x19_JHP"),
-                LIST_8("rhs_mag_30Rnd_556x45_Mk318_Stanag")
-            };
-            class Rank {
-                class PRIVATE {
-                    GRAD_FACTIONS_MEDICITEMS_CFR
-                };
-                class CORPORAL {
-                    GRAD_FACTIONS_MEDICITEMS_SQ
-                };
-                class SERGEANT {
-                    GRAD_FACTIONS_MEDICITEMS_PT
-                };
-                class LIEUTENANT: SERGEANT {};
-                class CAPTAIN: SERGEANT {};
-                class MAJOR: SERGEANT {};
-                class COLONEL: SERGEANT {};
-            };
-        };
-
-        //Explosive Specialist
-        class soldier_exp_F: Soldier_F {
-            headgear = "ARC_GER_Flecktarn_Helmet_EOD";
-            backpack = "ARC_GER_Kitbag_compact_Flecktarn";
-            addItemsToBackpack[] = {
-                "ACE_Clacker",
-                "ACE_M26_Clacker",
-                "SatchelCharge_Remote_Mag",
-                LIST_5("DemoCharge_Remote_Mag")
-            };
-        };
-
-        //Grenadier
-        class Soldier_GL_F: Soldier_F {
-            primaryWeapon = "rhs_weap_hk416d10_m320";
-            addItemsToVest[] = {
-                LIST_2("HandGrenade"),
-                LIST_2("SmokeShell"),
-                LIST_2("rhsusf_mag_17Rnd_9x19_JHP"),
-                LIST_8("rhs_mag_30Rnd_556x45_Mk318_Stanag"),
-                LIST_8("1Rnd_HE_Grenade_shell"),
-                LIST_3("1Rnd_SmokeRed_Grenade_shell")
-            };
-        };
-
-        //Rifleman (AT)
-        class soldier_LAT_F: Soldier_F {
-            secondaryWeapon = "rhs_weap_M136";
-        };
-
-        //Squad Leader
-        class Soldier_SL_F: Soldier_F {
-            backpack = "tf_rt1523g_big_bwmod";
-            addItemsToBackpack[] = {
-                LIST_2("SmokeShellBlue"),
-                LIST_2("SmokeShellGreen"),
-                LIST_2("SmokeShellOrange"),
-                LIST_2("SmokeShellPurple"),
-                LIST_2("SmokeShellRed"),
-                LIST_2("SmokeShellYellow"),
-                LIST_2("SmokeShell")
-            };
-        };
-
-        //Team Leader
         class Soldier_TL_F: Soldier_F {
-            primaryWeapon = "rhs_weap_hk416d10_m320";
-            addItemsToVest[] = {
-                LIST_2("HandGrenade"),
-                LIST_2("SmokeShell"),
-                LIST_2("rhsusf_mag_17Rnd_9x19_JHP"),
-                LIST_8("rhs_mag_30Rnd_556x45_Mk318_Stanag"),
-                LIST_2("1Rnd_SmokeRed_Grenade_shell"),
-                LIST_2("1Rnd_SmokeGreen_Grenade_shell"),
-                LIST_2("1Rnd_SmokeBlue_Grenade_shell"),
-                LIST_2("1Rnd_HE_Grenade_shell")
+            primaryWeapon = "";
+			primaryWeaponMagazine = "";
+			headgear = "H_MilCap_gen_F";
+        };
+		
+		class Soldier_TL_F: Soldier_F {
+			primaryWeapon = "rhs_weap_ak105_zenitco01";
+			primaryWeaponOptics = "rhs_acc_ekp8_02";
+			primaryWeaponMagazine = "rhs_30Rnd_545x39_7N10_AK";
+			uniform = "U_B_GEN_Soldier_F";
+			vest = "V_PlateCarrier2_blk";
+			goggles = "G_Balaclava_TI_G_blk_F";
+			headgear = "rhssaf_helmet_m97_black_nocamo";
+			
+			addItemsToVest[] = {
+                LIST_6("rhs_30Rnd_545x39_7N10_AK")
             };
         };
+		// POLIZEI ENDE
+		
+		//SECURITE ANFANG
+		
+		class Soldier_unarmed_F: Survivor_F {
+			addItemsToVest[] = {
+                LIST_4("6Rnd_45ACP_Cylinder")
+		}:
+		
+		class Soldier_lite_F: Soldier_unarmed_F {
+			primaryWeapon = "rhs_weap_vhsd2";
+			primaryWeaponOptics = "rhsusf_acc_T1_high";
+			primaryWeaponMagazine = "rhsgref_30rnd_556x45_vhs2";
+			addItemsToVest[] = {
+                LIST_6("rhsgref_30rnd_556x45_vhs2")
+		}:
+		
+		//SECURITE ENDE
+		
+		// MILITÄR ANFANG
+		
+		
+		class HeavyGunner_F: Survivor_F {
+			primaryWeapon = "rhs_weap_ak105_zenitco01_b33";
+			primaryWeaponMagazine = "rhs_30Rnd_545x39_7N10_AK";
+			primaryWeaponOptics = "hlc_optic_HensoldtZO_Lo"
+			uniform = "U_B_T_Soldier_AR_F";
+			vest = "V_PlateCarrierIAGL_oli";
+			backpack = "";
+			headgear = "rhs_altyn_visordown";
+			goggles = "G_Balaclava_oli";
+			handgunWeapon = "hgun_Rook40_F";
+			handgunWeaponMagazine = "16Rnd_9x21_Mag";
+			addItemsToVest[] = {
+                LIST_2("16Rnd_9x21_Mag"),
+				LIST_6("rhs_30Rnd_545x39_7N10_AK"),
+				LIST_2("HandGrenade"),
+				LIST_1 ("SmokeShell")
+            };
+		}
+		
+		
     };
 };
